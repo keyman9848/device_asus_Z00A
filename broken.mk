12eb61d37f67deb855410600097454fb2ca324fb
blkredstarV <blkredstar5@gmail.com>
2015-07-16 22:48:14 -0500

    Broke It
    Signed-off-by: blkredstarV <blkredstar5@gmail.com>

--- broken.mk
+++ broken.mk
@@ -2,16 +2,16 @@
 TARGET_SCREEN_HEIGHT := 1080
 TARGET_SCREEN_WIDTH := 1920
 
-# Inherit some common CM stuff.
+# Inherit some common Broken stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/moorefield/fhd/device.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/asus/moorefield/fhd/overlay
 
